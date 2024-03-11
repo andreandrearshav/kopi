c567585be1fdf1d9eaf0c14b92f87c9009f8de05
andrebagus <andreandrearshav@gmail.com>
2024-03-11 23:55:12 +0700

    menambahkan produk dan kontak

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// toggle class active
+// toggle class active humberger menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 
@@ -8,12 +8,72 @@ onclick = () =>{
     navbarNav.classList.toggle('active');
 };
 
+// toogle class untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox =document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick= (e) =>{
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// toogle class untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick =(e)=>{
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();    
+    
+};
+
+
+
+
+
+
 // klik di luar sidebar 
-const hamburger = document.querySelector
-('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target)&& !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
+  
+    if(!sb.contains(e.target)&& !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+    
+    if(!sc.contains(e.target)&& !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
+// modal box
+const itemDetailModal = document.querySelector ('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll ('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+
+    btn.onclick = (e) =>{
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault(); 
+    };
+});
+
+
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick =(e)=>{
+    itemDetailModal.style.display='none';
+    e.preventDefault();
+};
+// klik diluar modal
+const modal =document.querySelector('#item-detail-modal');
+window.onclick = (e) => {
+    if (e.target ===modal){
+       modal.style.display ='none';
+    }
+};
